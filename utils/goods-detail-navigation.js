8f8cbff19b4db03640428a185e0d630366aa14dd
Finjix <zf2857@gmail.com>
2026-08-16 14:23:42 +0800

    chore: 暂时禁用商品详情页跳转

---
+++ utils/goods-detail-navigation.js
@@ -0,0 +1,8 @@
+// 商品详情页暂时关闭，恢复时将开关改为 true。
+export const GOODS_DETAIL_NAVIGATION_ENABLED = false;
+
+export function navigateToGoodsDetail(url) {
+  if (!GOODS_DETAIL_NAVIGATION_ENABLED) return;
+
+  wx.navigateTo({ url });
+}
