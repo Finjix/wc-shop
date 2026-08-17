fcabe8e7e6bce8ec2514827a0ff4495ec71aba58
Finjix <zf2857@gmail.com>
2026-08-17 21:02:54 +0800

    chore: 解除对商品详情页的禁用

--- utils/goods-detail-navigation.js
+++ utils/goods-detail-navigation.js
@@ -1,5 +1,5 @@
 // 商品详情页暂时关闭，恢复时将开关改为 true。
-export const GOODS_DETAIL_NAVIGATION_ENABLED = false;
+export const GOODS_DETAIL_NAVIGATION_ENABLED = true;
 
 export function navigateToGoodsDetail(url) {
   if (!GOODS_DETAIL_NAVIGATION_ENABLED) return;
